e1e2992a4a53edfea26e2e56d9c03a99c8569ab3
user <juniorflex1406@gmail.com>
2025-06-29 20:38:15 -0300

    feat: atualizando o settings.py

--- social/settings.py
+++ social/settings.py
@@ -108,7 +108,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'socialapp', 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'socialapp', 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
